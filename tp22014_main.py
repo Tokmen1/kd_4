b78a4cfb9461e6acb9ce11e318c7b59a5ca31018
Tokmen1 <toms.polaks@inbox.lv>
2023-12-21 23:30:18 +0200

    Initial commit

---
+++ tp22014_main.py
@@ -0,0 +1,8 @@
+# This is a simple calculation program
+# created to demonstrate version control.
+from tp22014_libadd import *
+from tp22014_libmult import *
+a = 13
+b = 5
+print(plus(a,b))
+print(mult(a,b))
